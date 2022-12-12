742fb64a37365dacae97f55133a9094b7e48ab23
LukeArthurDLD <s172742@students.aie.edu.au>
2022-12-12 15:47:45 +1030

    Push

--- GameJam2022/Assets/Scripts/EnemyAI.cs
+++ GameJam2022/Assets/Scripts/EnemyAI.cs
@@ -3,16 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
-{
-    // Start is called before the first frame update
+{    
+    public enum AIType { Patrol, Chase, Guard };
+    public AIType type;
+
+    [Header("AI Properties")]
+    public float activeRange = 10f;
+    public float moveSpeed;
+    public float turnSpeed;
+    [Header("Attack Properties")]
+    public float attackRange = 1.0f;
+
+    public Transform groundCheck;
+    Player player;
+    int direction = 1;
     void Start()
     {
-        
+        player = Player.Instance;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Think();
+    }
+    void Think()
+    {
+        if (type == AIType.Patrol)
+        {
+            MoveEnemy();
+
+            RaycastHit hit;
+            Physics.Raycast(groundCheck.position, Vector3.down, out hit, 1f, LayerMask.GetMask("Ground"));
+
+            if (hit.collider == null)
+            {
+                TurnEnemy();
+            }
+            else
+            {
+               //MoveEnemy();
+            }
+
+        }
+        else if (type == AIType.Chase)
+        {
+
+        }
+        else if (type == AIType.Guard)
+        {
+
+        }
+    }
+    void MoveEnemy()
+    {
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime * direction);
+    }
+    void TurnEnemy()
+    {
+        direction *= -1;
     }
 }
--- GameJam2022/Assets/Scripts/Player.cs
+++ GameJam2022/Assets/Scripts/Player.cs
@@ -7,6 +7,14 @@ public class Player : MonoBehaviour
     public int maxHealth;
     public static int currentHealth;
     private Health health;
+    public static Player Instance;
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else if (Instance != this)
+            Destroy(gameObject);
+    }
     private void Start()
     {
         // get components
